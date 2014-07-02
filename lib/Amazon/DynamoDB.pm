0f74634ebe42f87ba86a382fcbd83c8db4fa077c
Rusty Conover <rusty@luckydinosaur.com>
2014-07-02 17:40:13 -0400

    Bump to version 0.17.
    Fix a typo in the API version default.

--- lib/Amazon/DynamoDB.pm
+++ lib/Amazon/DynamoDB.pm
@@ -74,7 +74,7 @@ sub new {
         Module::Load::load($args{implementation});
         $args{implementation} = $args{implementation}->new(%args);
     }
-    my $version = delete $args{version} || '201208010';
+    my $version = delete $args{version} || '20120810';
     my $pkg = __PACKAGE__ . '::' . $version;
     if (my $code = $pkg->can('new')) {
         $class = $pkg if $class eq __PACKAGE__;
